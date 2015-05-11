c56e8177ba5dce8acf28e80c36817853b7318f0a
Xiaoming Yang <xmyang@codeaurora.org>
2015-05-11 13:48:49 +0800

    NuPlayer: Clear stale track packets on seek
    EOS isn't notified to nuplayer immediately upon
    seek. Stale packets before seeking is still held
    by track, thus EOS won't be returned until all
    packets are consumed.
    
    To fix this issue and align nuplayer's behavior
    with awesome player, stale packets needs to be
    cleared on seek.
    
    Change-Id: I00d825cad7a9f3de1daf494078e7379dca8eba2e
    CRs-Fixed: 828785

--- media/libmediaplayerservice/nuplayer/GenericSource.cpp
+++ media/libmediaplayerservice/nuplayer/GenericSource.cpp
@@ -1572,6 +1572,7 @@ void NuPlayer::GenericSource::readBuffer(
     if (seekTimeUs >= 0) {
         options.setSeekTo(seekTimeUs, MediaSource::ReadOptions::SEEK_PREVIOUS_SYNC);
         seeking = true;
+        track->mPackets->clear();
     }
 
     if (mIsWidevine) {
